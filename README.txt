efa7e746a923db47350f2756634889dcbcf22886
AshokSivathayalan <120435769+AshokSivathayalan@users.noreply.github.com>
2024-03-18 20:56:15 -0400

    Added README, db script

---
+++ README.txt
@@ -0,0 +1,10 @@
+SETTING UP DATABASE:
+
+Needs Postgresql & pgAdmin4. 
+
+RUNNING PYTHON PROGRAM:
+Needs psycopg python module installed with pip.
+If not, run pip install "psycopg[binary]"
+
+Open command line in folder where python program is stored, and run 
+py assignment3.py
---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE students(student_id SERIAL PRIMARY KEY, first_name VARCHAR(20), last_name VARCHAR(30), email VARCHAR(50), enrollment_date DATE);
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
